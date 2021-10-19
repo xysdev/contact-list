6bdda9c04019c2fe9057117c968fef17cbacbe81
xyster <geramian.erfan@gmail.com>
2021-10-19 14:51:26 +0330

    add get contact api

--- src/api/contactsApi.js
+++ src/api/contactsApi.js
@@ -1,7 +1,5 @@
 import api from './../services/networkService';
 export const getContactsApi = async () => {
-    debugger
-    console.log(api)
     const result = await api.get('/seg');
     return result;
 };
