eb0ec8ca5dcb1d2819346f9c9b5b1bf26ce85810
erfan <geramian.erfan@outlook.com>
2021-10-24 00:31:33 +0330

    Add try catch block to the code

--- src/api/contactsApi.js
+++ src/api/contactsApi.js
@@ -1,6 +1,11 @@
 import { config } from 'config/config';
 import api from 'services/networkService';
 export const getContactsApi = async () => {
-    const result = await api.get('', { params: { results: config.contactCount, nat: config.culture.nat } });
-    return result;
+    try{
+        const result = await api.get('', { params: { results: config.contactCount, nat: config.culture.nat } });
+        return result;
+    }catch(error){
+        throw error;
+    }
+    
 };
