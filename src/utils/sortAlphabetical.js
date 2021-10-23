fb3b781ba3e6fcd76326b159a7f926ce73050362
erfan <geramian.erfan@outlook.com>
2021-10-23 16:49:00 +0330

    fixing sort that it sort insied of each letter

--- src/utils/sortAlphabetical.js
+++ src/utils/sortAlphabetical.js
@@ -1,12 +1,25 @@
 export const sortAlphabetical = (array) => {
+    ///TODO: replace with insertion sort
     const result = {};
     if (array == null) return result;
     array.forEach((item) => {
-        const letter= item.name.last[0].toLocaleLowerCase()
+        const letter = item.name.last[0].toLocaleLowerCase();
         if (!result[letter]) {
             result[[letter]] = [];
         }
         result[letter].push(item);
+    });
+    Object.keys(result).forEach((index) => {
+        result[index].sort(function (a, b) {
+            debugger
+            if (a.name.last < b.name.last) {
+                return -1;
+            }
+            if (a.name.last > b.name.last) {
+                return 1;
+            }
+            return 0;
+        });
     });
     return result;
 };
